6de334fab8eca91fc61eb933e9d0927837d91801
Iraq Jaber <iraq+github@zitadel.com>
2025-08-22 21:07:12 +0100

    feat(adding connections): allow custom id for new connections

--- lua/dbee/sources.lua
+++ lua/dbee/sources.lua
@@ -101,7 +101,9 @@ function sources.FileSource:create(conn)
   -- read from file
   local existing = self:load()
 
-  conn.id = "file_source_/" .. utils.random_string()
+  if conn.id == nil or conn.id == "" then
+    conn.id = "file_source_/" .. utils.random_string()
+  end
   table.insert(existing, conn)
 
   -- write back to file
